528b4950f635b4377bd217b07fe90f25ef957cbb
sharif <sharifrahman9@gmail.com>
2025-10-22 20:27:10 +0600

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "node:path";
 import path from "path";
 
 // https://vite.dev/config/
@@ -16,6 +17,14 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(path.resolve(), "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(path.resolve(), "index.html"),
+        pages: resolve(path.resolve(), "pages/index.html"),
+      },
     },
   },
 });
